32b0d08df242b872c613e7f3f7c2d997d0b441fe
PraveenKumar2612 <praveenash2002@gmail.com>
2024-12-31 08:03:03 +0530

    add images and variable interpolation

--- dj_app/views.py
+++ dj_app/views.py
@@ -3,4 +3,41 @@ from django.shortcuts import render
 # Create your views here.
 
 def net_demo(request):
-    return render(request=request,template_name='main.html')
+    top_movies_list=[
+        {
+        "cardbodytext":"this is image 2",
+        "cardimg":'images/pic 2.jpeg'
+    },
+        {
+        "cardbodytext":"this is image 3",
+        "cardimg":'images/pic 3.jpeg'
+    },
+        {
+        "cardbodytext":"this is image 4",
+        "cardimg":'images/pic 4.jpeg'
+    },
+        {
+        "cardbodytext":"this is image 5",
+        "cardimg":'images/pic 5.jpeg'
+    },
+        {
+        "cardbodytext":"this is image 6",
+        "cardimg":'images/pic 6.jpeg'
+    },
+    #     {
+    #     "cardbodytext":"this is image 2",
+    #     "cardimg":'images/pic 2.jpeg'
+    # },
+    #     {
+    #     "cardbodytext":"this is image 3",
+    #     "cardimg":'images/pic 3.jpeg'
+    # },
+
+    ]
+    
+    
+    
+    data={
+       "content":top_movies_list
+    }
+    return render(request=request,template_name='main.html',context=data)
